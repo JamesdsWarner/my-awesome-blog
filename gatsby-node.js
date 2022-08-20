0abfcae673214cf92c326fcd57e242ee493b9d88
JamesdsWarner <jamesdswarner@gmail.com>
2022-08-20 22:47:25 +0100

    adding create slug onCreateNode

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,19 @@
 // exports.createPages = async ({ graphql, actions }) => {
 //   const { data } = await graphql(``)
 // }
+
+const path = require(`path`)
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
